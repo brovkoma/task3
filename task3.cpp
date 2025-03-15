4d2a47fe3131e9a9a6e39ac0b7cc31c6b6c4ad72
unknown <brovkoma18@gmail.com>
2025-03-15 19:07:53 +0400

    fibonacci

--- task3.cpp
+++ task3.cpp
@@ -2,7 +2,20 @@
 
 using namespace std;
 
+int f(int n) {
+    if (n == 1 || n == 2) {
+        return 1;
+    }
+    else {
+        return f(n - 1) + f(n - 2);
+    }
+}
+
 int main()
 {
-    cout << "Hello, World!\n";
+    setlocale(LC_ALL, "rus");
+    cout << "Введите n" << endl;
+    int n;
+    cin >> n;
+    cout << f(n) << endl;
 }
